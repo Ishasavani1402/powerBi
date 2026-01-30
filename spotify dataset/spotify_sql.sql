df4ec0ecf6fb489b14592feac7cbe867817c4902
Ishasavani1402 <savaniisha57@gmail.com>
2026-01-30 11:53:28 +0530

    save changes

--- spotify dataset/spotify_sql.sql
+++ spotify dataset/spotify_sql.sql
@@ -1,5 +1,7 @@
 create database spotify;
 use spotify;
 select * from artist;
-
+select count(artist_id) as total_artist from artist;
 select name , popularity , dense_rank() over(order by popularity desc) as d_rank from artist;
+select name , followers, dense_rank() over(order by followers desc) as d_rank from artist;
+select count(main_genre) from artist;
